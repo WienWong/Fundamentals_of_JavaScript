71a7b24ae9979183e757920b8d594182117d235d
WienWong <wangzichengfu@gmail.com>
2015-03-08 11:34:28 +0100

    Update JS_Lecture5.js

--- JS_Lecture5.js
+++ JS_Lecture5.js
@@ -133,11 +133,73 @@ var happy = function() {
   }
 };
 
-//
+// Adventure 2
 
+// 1. Prompt the user for input and store it in a variable called user. 
+var user=prompt("What's your hobby");
 
+// 2. Call either .toUpperCase() or .toLowerCase() on your prompt to ensure that the input you 
+// get from the user is capitalized the way you expect.
+var user=prompt("What's your hobby").toUpperCase();
 
+// 3. Add a switch statement that will test for several different cases (that is, different possible user inputs). 
 
+var user=prompt("What's your hobby").toUpperCase();
+
+switch(user){
+    case "Programming".toUpperCase():
+        console.log("This is fantastic!");
+        break;
+    case "Swimming".toUpperCase():
+        console.log("Great!");
+        break;
+    case "Meditation!".toUpperCase():
+        console.log("Awesome!");
+        break;
+    case "Football game".toUpperCase():
+        console.log("Same as me");
+        break;
+    default:
+        console.log("I don't like your hobby.")
+    
+}
+
+// 4. Add some if/else statements to your cases that check to see whether one condition and 
+// another condition are true, as well as whether one condition or another condition are true. 
+// Use && and || at least one time each.
+var troll = prompt("You're walking through the forest, minding your own business, and you run into a troll! Do you FIGHT him, PAY him, or RUN?").toUpperCase();
+
+switch(troll) {
+  case 'FIGHT':
+    var strong = prompt("How courageous! Are you strong (YES or NO)?").toUpperCase();
+    var smart = prompt("Are you smart?").toUpperCase();
+    if(strong === 'YES' || smart === 'YES') {
+      console.log("You only need one of the two! You beat the troll--nice work!");
+    } else {
+      console.log("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a troll. You lose!");
+    }
+    break;
+  case 'PAY':
+    var money = prompt("All right, we'll pay the troll. Do you have any money (YES or NO)?").toUpperCase();
+    var dollars = prompt("Is your money in Troll Dollars?").toUpperCase();
+    if(money === 'YES' && dollars === 'YES') {
+      console.log("Great! You pay the troll and continue on your merry way.");
+    } else {
+      console.log("Dang! This troll only takes Troll Dollars. You get whomped!");
+    }
+    break;
+  case 'RUN':
+    var fast = prompt("Let's book it! Are you fast (YES or NO)?").toUpperCase();
+    var headStart = prompt("Did you get a head start?").toUpperCase();
+    if(fast === 'YES' || headStart === 'YES') {
+      console.log("You got away--barely! You live to stroll through the forest another day.");
+    } else {
+      console.log("You're not fast and you didn't get a head start? You never had a chance! The troll eats you.");
+    }
+    break;
+  default:
+    console.log("I didn't understand your choice. Hit Run and try again, this time picking FIGHT, PAY, or RUN!");
+}
 
 
 
